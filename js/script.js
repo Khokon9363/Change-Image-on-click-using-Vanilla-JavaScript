7ef195ae5067454e6f3eb079bca9f1c0c4379db9
khokon9363 <khokon9363@gmail.com>
2021-05-24 13:28:41 +0600

    Change Image on click using Vanilla JavaScript

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function change1(){
+    document.getElementById('pic').src='img/2.png';
+}
+
+function change2(){
+    document.getElementById('pic').src='img/3.png';
+}
+
+function change3(){
+    document.getElementById('pic').src='img/4.png';
+}
+
+function change4(){
+    document.getElementById('pic').src='img/5.png';
+}
+
+function change5(){
+    document.getElementById('pic').src='img/6.png';
+}
